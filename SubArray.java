ab7c20acf9d472a2dcfdce2ea7602cccbe70766c
kumari_mahi <mahimthkumari43@gmail.com>
2026-09-07 17:44:34 +0000

    new file:   SubArray.java

---
+++ SubArray.java
@@ -0,0 +1,20 @@
+public class SubArray {
+    public static void PrintSubArray(int num[]){
+        for(int i =0;i<num.length;i++){
+            int start = i;
+            for(int j = i;j<num.length;j++){
+                int end = j;
+                for(int k = start;k<end;k++){
+                    System.out.print(num[k]+" ");
+                }
+                System.out.println();
+            }
+            //System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2,3,4,7,8,9};
+        PrintSubArray(numbers);
+        
+    }
+}
